62af8c8a40409b20958cee6663aaf971cc5bfc0d
you never have to do your homework again <cowgoesmoo@users.noreply.github.com>
2017-07-02 19:06:26 +0800

    Update question1.java

--- pracitcal6/question1.java
+++ pracitcal6/question1.java
@@ -5,22 +5,24 @@
  */
 package PROG.pracitcal6;
 
-import javax.swing.JOptionPane;
+import javax.swing.JOptionPane; 
 /**
  *
  * @author cowgoesmoo <thesendmenudes27@gmail.com>
  */
 public class question1 {
-    static void printtable(int x) {
-        String y = "";
+    static void printTable(int x) {
+        
+        String y = ""; //initialise y
+        
         for (int i = 1; i <= 12; i++) {
             y += x + " x " + i + " = " + i * x + "\n";
-            
-        }
+        }//end of for loop
+        
         JOptionPane.showMessageDialog(null, y);
-    }
+    }//end of method printTable
     
     public static void main(String[] args) {
         printtable(5);
-    }
+    } //end of main
 }
